b84578040a165b1e19d615d4dcd7579e6700345d
Alex Kwan <hkustvampire@gmail.com>
2017-01-16 22:41:35 +0800

    Not to mount to the database file, but a database director

--- init.sh
+++ init.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-echo "First arg: $1"
-echo "Second arg: $2"
+configdir=$1
+keysdir=$2
 
-config=$1
-key=$2
+echo docker run --name KeePass2 -v $configdir:/root/.config -v $keysdir:/root/db biglioncoding/gui-keepass2
 
-echo docker run --name KeePass2 -v $config:/root/.config -v $key:/root/keys.kdbx  biglioncoding/gui-keepass2
-
-docker run --name KeePass2 -v $config:/root/.config -v $key:/root/keys.kdbx  biglioncoding/gui-keepass2
+docker run --name KeePass2 -v $configdir:/root/.config -v $keysdir:/root/db biglioncoding/gui-keepass2
